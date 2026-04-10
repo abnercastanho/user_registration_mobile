302d388d85a2df0b3f3e39df9d2e347823c1394d
Abner Cardoso <cardosoabner64@gmail.com>
2026-04-09 21:13:23 -0300

    Criado a classe principal da atividade de cadastro de usuários

--- user_registration-mobile-main/app/build.gradle.kts
+++ user_registration-mobile-main/app/build.gradle.kts
@@ -41,7 +41,15 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.room.common.jvm)
+    implementation(libs.room.runtime)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    //Bliblioteca principal
+    implementation("androidx.room.room.-runtime:2.6.1")
+
+    //Processador de anotações de Room (Necessario para gerar codigo automaticamente)
+    annotationProcessor("androidx.room.room-compiler:2.6.1")
+    annotationProcessor(libs.room.compiler)
 }
